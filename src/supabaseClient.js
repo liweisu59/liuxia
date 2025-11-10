067211d79cecc095b6adb329229a303cf74db739
liweisu <liweisu59@gmail.com>
2025-11-10 10:53:35 +0800

    Update Supabase client key to use VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Get the URL&key from .env.local
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY
 
 // Create and export the Supabase client
 export const supabase = createClient(supabaseUrl, supabaseKey)
